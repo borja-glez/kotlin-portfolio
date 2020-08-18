c555b105f499fc9807031c4a26f777e00e9d6648
borja-glez <borja@borjaglez.com>
2020-08-18 20:21:49 +0200

    Quiz App

---
+++ QuizApp/app/src/main/java/com/borjaglez/quizapp/ResultActivity.kt
@@ -0,0 +1,24 @@
+package com.borjaglez.quizapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_result.*
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+
+        tv_name.text = intent.getStringExtra(Constants.USER_NAME)
+
+        val correctAnswers = intent.getIntExtra(Constants.CORRECT_ANSWERS, 0)
+        val totalQuestions = intent.getIntExtra(Constants.TOTAL_QUESTIONS, 0)
+        tv_score.text = "Your Score is $correctAnswers out of $totalQuestions"
+
+        btn_finish.setOnClickListener{
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+    }
+}
